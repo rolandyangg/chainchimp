e3e9cab711e456f30f574bbf2724e0dffb46a829
roland yang <rolandyang@gmail.com>
2023-04-23 02:50:34 -0700

    Dashboard Progress

--- client/src/components/dashboard/dashboard.js
+++ client/src/components/dashboard/dashboard.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {
   ChakraProvider,
   Center,
+  Card,
+  CardBody,
   Box,
   Text,
   Flex,
@@ -15,11 +17,31 @@ import {
   Heading,
   Icon,
   HStack,
+  Progress,
   useBreakpointValue,
+  Divider,
 } from '@chakra-ui/react';
 import { FaGithub } from 'react-icons/fa'
 import Footer from '../footer.js'
 import Navbar from '../navbar.js'
+
+function ProductCard({name, id, stage, party, progress}) {
+  return (<>
+   <Card mx="10px" my="20px" overflow='hidden' variant='outline' maxW="90%" bgColor="blackAlpha.300" color="white">
+            <CardBody mt="-7px">
+            <Flex align="center" justify="space-between" w="100%">
+                <VStack align="start" h="90px" w="80%">
+                    
+                    <Text>Name: {name} | ID: {id} | Party: {party} | Stage: {stage} </Text>
+                    
+                    <Progress height="16px" color="white" w="100%" value={progress}/>
+                    
+                </VStack>
+                <Button variant='outline' mx="5px" my="5px" _hover={{ backgroundColor: "blue.800", borderColor: "blue.300" }}>View History</Button>
+                </Flex>
+            </CardBody>
+        </Card></>)
+}
 
 export default function Dashboard() {
   return (
@@ -28,12 +50,21 @@ export default function Dashboard() {
       {/** NAVBAR */}
       <Navbar/>
 
-    {/** ISSUES/PROBLEMS WITH THE CURRENT SYSTEM */}
+      <Center m={{base: "25px", md: "50px"}}>
+                <VStack border="1px" p="10px" rounded={7} w="95%" maxW="1100px" minH="600px">
+                    <Heading py="10px">Products</Heading>
+                    <Divider/>
+                    <Box justify="center" py="10px" fontSize="xl">
+                        {/* <Text fontWeight="bold" fontSize="2xl">test</Text> */}
+                        <ProductCard name="Laptop" id="10" stage="Manufacturer" party="9123912931293123x1239123" progress="40"/>
+                        <ProductCard name="T-Shirts" id="13" stage="Consumer" party="20301203102xasod02" progress="100"/>
+                        <ProductCard name="Beds" id="3" stage="Supplier" party="23020302303013kkk" progress="60"/>
+                    </Box>
+                </VStack>
+            </Center>
 
-    {/** SOLUTION/HOW IT WORKS */}
-
-    {/** FOOTER */}
-    <Footer/>
+      {/** FOOTER */}
+      <Footer/>
     </>
   );
 }
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -28,11 +28,11 @@ export default function Navbar() {
         <Box maxW="1100px" w="100%" mx="25px" pt={{base: 0, sm: 0}}>
             <Flex align="center" justify="space-between" flexDir={{ base: "column", md: "row" }} p="20px">
                 <Flex>
-                    <Link href="/"><Heading fontSize="2xl">epic supply chain blockchain project</Heading></Link>
+                    <Link href="/"><Heading fontSize="2xl">ChainChimp</Heading></Link>
                 </Flex>
                 <HStack spacing={6} flexWrap="wrap" justify="center">
-                    <Link href="/" _hover={{color: "white"}}><Text color="whiteAlpha.700" fontSize="xl" _hover={{color: "white"}}>home</Text></Link>
-                    <Link href="/Dashboard" _hover={{color: "white"}}><Text color="whiteAlpha.700" fontSize="xl" _hover={{color: "white"}}>dashboard</Text></Link>
+                    <Link href="/" _hover={{color: "white"}}><Text color="whiteAlpha.700" fontSize="xl" _hover={{color: "white"}}>Home</Text></Link>
+                    <Link href="/Dashboard" _hover={{color: "white"}}><Text color="whiteAlpha.700" fontSize="xl" _hover={{color: "white"}}>Dashboard</Text></Link>
                 </HStack>
             </Flex>
         </Box>
