92487bd8990175424e80e22b0defa85b0f75b116
roland yang <rolandyang@gmail.com>
2023-04-23 22:22:10 -0700

    Slight Dashboard Changes
    Added tabs and refactored code

---
+++ client/src/components/dashboardproduct.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  Text,
+  Flex,
+  Card,
+  CardBody,
+  VStack,
+  Progress,
+  Button
+} from '@chakra-ui/react';
+
+export default function ProductCard({name, id, stage, party, progress}) {
+  return (<>
+   <Card mx="10px" my="20px" overflow='hidden' variant='outline' maxW="90%" bgColor="blackAlpha.300" color="white">
+            <CardBody mt="-7px">
+            <Flex align="center" justify="space-between" w="100%">
+                <VStack align="start" h="90px" w="80%">
+                    
+                    <Text>Name: {name} | ID: {id} | Party: {party} | Stage: {stage} </Text>
+                    
+                    <Progress align="left" height="16px" color="white" w="100%" value={progress}/>
+                    
+                </VStack>
+                <Button variant='outline' mx="5px" my="5px" _hover={{ backgroundColor: "blue.800", borderColor: "blue.300" }}>View History</Button>
+                </Flex>
+            </CardBody>
+        </Card></>)
+}
