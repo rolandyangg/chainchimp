c339305644a19ea88182c56e863d8335af8f66ef
roland yang <rolandyang@gmail.com>
2023-04-24 03:42:00 -0700

    forgor comma

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,13 +1,14 @@
 const STAGE_TO_NUM = new Map();
 
-STAGE_TO_NUM.set("Raw Materials", 0);
-STAGE_TO_NUM.set("Supplier", 1);
-STAGE_TO_NUM.set("Manufacturer", 2);
-STAGE_TO_NUM.set("Distributor", 3);
-STAGE_TO_NUM.set("Consumer", 4);
+STAGE_TO_NUM.set("Supply Chain Manager", 0);
+STAGE_TO_NUM.set("Raw Materials", 1);
+STAGE_TO_NUM.set("Supplier", 2);
+STAGE_TO_NUM.set("Manufacturer", 3);
+STAGE_TO_NUM.set("Distributor", 4);
+STAGE_TO_NUM.set("Consumer", 5);
 
 const NUM_TO_STAGE = new Map([
-    [0, "Supply Chain Manager"]
+    [0, "Supply Chain Manager"],
     [1, "Raw Materials"],
     [2, "Supplier"],
     [3, "Manufacturer"],
